85205ae3cab104802683aa4c712f1036df31317e
kora13481 <amelianekor@gmail.com>
2023-09-23 12:46:28 +0200

    nickel

--- src/app/views/online/online.component.ts
+++ src/app/views/online/online.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IMenuItem } from './types/navigation';
+import { IChildItem, IMenuItem } from './types/navigation';
 import { OnlineService } from './online.service';
 
 @Component({
@@ -8,24 +8,64 @@ import { OnlineService } from './online.service';
   styleUrls: ['./online.component.css']
 })
 export class OnlineComponent implements OnInit {
+  showElement: boolean = true;
+  showMenu: boolean = false;
+  showSideBar: boolean = false;
+
+  selectedItem!: IMenuItem;
   nav: IMenuItem[] = [];
+  subMenu: IChildItem[] = [];
 
   constructor(private onlineService: OnlineService) { }
 
   ngOnInit() {
     // Initialize the 'nav' property with data from the 'onlineService'
     this.nav = this.onlineService.IMenuItem;
+
+    this.showElement = false;
+    this.showMenu = true;
+    this.showSideBar = true;
+
   }
 
-  toggleSubMenu(index: number) {
-    // Toggle the active state of the clicked item
-    this.nav[index].active = !this.nav[index].active;
+  selectItem(item: IMenuItem) {
+    if (this.selectedItem === item) {
+        this.showElement = true;
+    } else {
+        this.nav.forEach(i => {
+            i.active = i === item; // Définir l'état actif de chaque élément principal de menu
+        });
+        this.selectedItem = item;
+        this.setActiveMainItem(item);
 
-    // Ensure that all other items are deactivated
-    for (let i = 0; i < this.nav.length; i++) {
-      if (i !== index) {
-        this.nav[i].active = false;
-      }
+        // Ajoutez cette ligne pour afficher l'état de la sous-rubrique en console log
+        console.log('Sous-rubrique sélectionnée :', item);
     }
   }
+
+  closeChildNav() {
+    // this.navService.sidebarState.childnavOpen = false;
+    this.showElement = false;
+    this.showMenu = true;
+}
+
+setActiveMainItem(item: IMenuItem) {
+  this.nav.forEach(i => {
+      i.active = i === item;
+  });
+  this.showElement = !this.showElement;
+  this.showMenu = !this.showMenu;
+  
+  if (item.sub) {
+      this.subMenu = item.sub;
+  } else {
+      this.subMenu = [];
+  }
+}
+
+onClickChangeActiveFlag(item: IMenuItem) {
+    this.setActiveMainItem(item);
+}
+
+  
 }
